1b40bd40329412684fc56f532206f0cd13630fa6
sergey280680 <sergey280680h@gmail.com>
2021-08-28 00:24:04 +0300

    проверка

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -42,7 +42,6 @@ Event.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.number.isRequired,
   followers: PropTypes.number.isRequired,
   views: PropTypes.number.isRequired,
   likes: PropTypes.number.isRequired,
