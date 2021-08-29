cf184f8d2f64d8c3b2749e87992581232ec266c1
sergey280680 <sergey280680h@gmail.com>
2021-08-29 11:18:58 +0300

    transaction table created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import friends from "../friends.json";
 import { UserBoard } from "components/UserBoard/UserBoard";
 import statisticalData from "../statistical-data.json";
 import { Statistics } from "components/Statistics/Statistics";
+import transactions from "../transaction-history.json";
+import { Transactions } from "components/Transactions/Transactions";
 import user from "../user.json";
 import { FriendsBoard } from "components/FriendsBoard/FriendsBoard";
 
@@ -10,6 +12,7 @@ export const App = () => {
     <>
       <FriendsBoard events={friends} />
       <Statistics title="Upload stats" items={statisticalData} />
+      <Transactions events={transactions} />
       <UserBoard event={user} />
     </>
   );
---
+++ src/components/Transactions/Transactions.jsx
@@ -0,0 +1,38 @@
+//
+import PropTypes from "prop-types";
+import css from "./Transactions.module.css";
+
+export const Transactions = ({ events }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {events.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+Transactions.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
